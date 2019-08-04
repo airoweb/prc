4312af6e47edf05a4ff9db794405525fbb3346b5
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-08-05 00:44:09 +0430

    add schedule setting

--- script.js
+++ script.js
@@ -3,27 +3,36 @@ angular.module('Airofarm', ['ngMaterial', 'ngMessages'])
   .run(['$rootScope', '$http', function ($rootScope, $http) {
     $http.get('https://raw.githubusercontent.com/3ehzad/prc/master/ipsetting.json?_=' + Date.now(), { cache: false }).then(function (data) {
       $rootScope.ip = data.data;
-      // $rootScope.espip = $rootScope.ip[0].espip;
-      $rootScope.espip = 'http://192.168.1.11/'
-      // $rootScope.devip = $rootScope.ip[0].devip;
-      $rootScope.devip = "http://192.168.1."
+      //local
+      // $rootScope.espip = 'http://192.168.1.11/'
+      // $rootScope.devip = "http://192.168.1."
+      //online
+      $rootScope.espip = $rootScope.ip[0].espip;
+      $rootScope.devip = $rootScope.ip[0].devip;
+    });
+    // $http.get('scheduletime.json', { cache: false }).then(function (data) {
+    $http.get('https://raw.githubusercontent.com/3ehzad/prc/master/scheduletime.json?_=' + Date.now(), { cache: false }).then(function (data) {
+      $rootScope.scheduletime = data.data;
     });
   }])
 
   .controller('minerstatusController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
     $scope.psuStatus = function () {
-      $http.get($rootScope.espip + 'PSU_STATUS', { cache: false }).then(function (data) {
+      // $http.get('psustatus.json', { cache: false }).then(function (data) {
+        $http.get($rootScope.espip + 'PSU_STATUS', { cache: false }).then(function (data) {
         $scope.minerstatus = data.data;
         // $window.location.reload();
       });
-  }
+    }
     // run commands
     $scope.psuRUN = function () {
+      console.log("schedule run!")
       $http.get($rootScope.espip + 'MINER_RUN').then(function (response) {
         $scope.psuresponse = response.data;
       });
     }
     $scope.psuSTOP = function () {
+      console.log("schedule stop!")
       $http.get($rootScope.espip + 'MINER_STOP').then(function (response) {
         $scope.psuresponse = response.data;
       });
@@ -53,6 +62,27 @@ angular.module('Airofarm', ['ngMaterial', 'ngMessages'])
         $scope.camresponse = response.data;
       });
     }
+    // Schedule off setting
+    $scope.scheduleEnableoff = function () {
+      var now = new Date();
+      var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), $rootScope.scheduletime.off_h, $rootScope.scheduletime.off_m, 0, 0) - now;
+      if (millisTill10 < 0) {
+        millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+      }
+      $scope.millisTill10 = millisTill10;
+      setTimeout(function () { $scope.psuSTOP() }, millisTill10);
+    }
+    // Schedule on setting
+    $scope.scheduleEnableon = function () {
+      var now = new Date();
+      var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), $rootScope.scheduletime.on_h, $rootScope.scheduletime.on_m, 0, 0) - now;
+      if (millisTill10 < 0) {
+        millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+      }
+      $scope.millisTill10 = millisTill10;
+      setTimeout(function () { $scope.psuRUN() }, millisTill10);
+    }
+    // end Scheduling setting
   }])
   .controller('deviceController', ['$scope', '$rootScope', '$http', '$mdDialog', '$window', '$location', '$timeout', function ($scope, $rootScope, $http, $mdDialog, $window, $location, $timeout) {
     $scope.isLoading = true;
@@ -86,7 +116,7 @@ angular.module('Airofarm', ['ngMaterial', 'ngMessages'])
       $window.location.reload();
     }
     $scope.relayStatus = function () {
-    $http.get($rootScope.espip + 'RELAY_STATUS', { cache: false }).then(function (data) {
+      $http.get($rootScope.espip + 'RELAY_STATUS', { cache: false }).then(function (data) {
         $rootScope.relaystatus = data.data;
       });
     }
