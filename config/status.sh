6105f6e0f5b43cf95bef5ae2ff9a34a5fda7409b
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-10-12 02:02:28 +0330

    modify: sh

--- config/status.sh
+++ config/status.sh
@@ -7,17 +7,8 @@
 # Replace passwords, IP's and email address as needed
 
 # Get the current hash rate
-hash_rate=$(sshpass -p "Hamid1010" ssh -o StrictHostKeyChecking=no root@192.168.7.101 "cgminer-api | grep '\[GHS av\]' | cut -b 16-20")
+cgminer `echo '{"command":"stats"}' | nc 192.168.7.101 4028 >> output.txt`
+
+hash_rate=$("grep '\[GHS av\]' output.txt | cut -b 16-20")
 epoch=$(date +%s)
-
-# Notify me and reboot if its low
-if [ 18000 -gt "$hash_rate" ];  then
-	echo "Hashrate is $hash_rate, rebooting..." | mail -s "Miner Status - $epoch" behzadamirinezhad@gmail.com;
-	sshpass -p "Hamid1010" ssh -o StrictHostKeyChecking=no root@192.168.7.101 "/sbin/reboot"
-
-	# A ghetto health check
-	sleep 90
-
-	uptime=$(sshpass -p "Hamid1010" ssh -o StrictHostKeyChecking=no root@192.168.7.101 "uptime")
-	echo "Current uptime is: $uptime." | mail -s "Miner Status - $epoch" behzadamirinezhad@gmail.com;
-fi
+echo "Hashrate is $hash_rate
