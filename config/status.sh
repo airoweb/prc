bde102119938fc21f3f2818ce19fbd2d43dc3733
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-10-12 03:06:11 +0330

    bug: last code

--- config/status.sh
+++ config/status.sh
@@ -10,9 +10,9 @@
 `echo '{"command":"stats"}' | nc 192.168.7.101 4028 >> output.txt` |
 
 hash_rate=$(awk -v tag='GHS 5s' 'match($0,"\""tag"\": *(\"[^\"]*|[0-9]+)") { val=substr($0,RSTART,RLENGTH); sub(/^"[^"]+": *"?/,"",val); print val }' output.txt) |
-epoch=$(date +%s) | 
-echo "Hashrate is $hash_rate |
-if [ 18000 -gt "$hash_rate" ];  then |
-	echo "Hashrate is $hash_rate, rebooting..." | mail -s "Antminer Status - $epoch" behzadamirinezhad@gmail.com; |
-	"/etc/init.d/cgminer reload" |
+epoch=$(date +%s) |
+if [ "18000" -gt "$hash_rate" ];  then 
+	echo "Hashrate is $hash_rate, rebooting..." | mail -s "Antminer Status - $epoch" behzadamirinezhad@gmail.com;
+	"/etc/init.d/cgminer reload"
+    echo "Need reload"
 fi
