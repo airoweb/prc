84fd4568a41892ea5e6163f665ffba5b9c864a59
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-10-12 14:00:50 +0330

    fix: some edit

--- config/status.sh
+++ config/status.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-# This bash script is used to login to the antminer, determine hash rate, check if its below a threshold.
-# If it is then reboot the miner, email the hashrate and the event.
-# Wait 90 seconds (per the antminer docs), email out an uptime to verify that its resumed.
-# Using sshpass since my version of antminer wipes all ssh keys upon reboot
-# Replace passwords, IP's and email address as needed
-
-# Get the current hash rate
+`rm output.txt` |
 `echo '{"command":"stats"}' | nc 192.168.7.101 4028 >> output.txt` |
 
 hash_rate=$(awk -v tag='GHS av' 'match($0,"\""tag"\": *(\"[^\"]*|[0-9]+)") { val=substr($0,RSTART,RLENGTH); sub(/^"[^"]+": *"?/,"",val); print val }' output.txt) |
