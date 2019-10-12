47da405208d05ab60bf7479ceb5f252189798cb6
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-10-12 14:02:10 +0330

    fix: some change

--- config/status.sh
+++ config/status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-`rm output.txt` |
+rm output.txt |
 `echo '{"command":"stats"}' | nc 192.168.7.101 4028 >> output.txt` |
 
 hash_rate=$(awk -v tag='GHS av' 'match($0,"\""tag"\": *(\"[^\"]*|[0-9]+)") { val=substr($0,RSTART,RLENGTH); sub(/^"[^"]+": *"?/,"",val); print val }' output.txt) |
 epoch=$(date +%s) |
 if [ "18000" -gt "$hash_rate" ];  then 
-	echo "Hashrate is $hash_rate, rebooting..." | mail -s "Antminer Status - $epoch" behzadamirinezhad@gmail.com;
+	echo "Hashrate is $hash_rate, rebooting..." | mail -s "Device Status - $epoch" behzadamirinezhad@gmail.com;
 	"/etc/init.d/cgminer reload"
 fi
