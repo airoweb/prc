1524538295f8e3b0446d433dba5957b9eb4bbbe3
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-10-12 14:42:19 +0330

    fix: bash script is ok

--- config/status.sh
+++ config/status.sh
@@ -5,6 +5,6 @@ rm output.txt |
 
 hash_rate=$(awk -v tag='GHS av' 'match($0,"\""tag"\": *(\"[^\"]*|[0-9]+)") { val=substr($0,RSTART,RLENGTH); sub(/^"[^"]+": *"?/,"",val); print val }' output.txt) |
 epoch=$(date +%s) |
-if [ 18000 > hash_rate ];  then 
+if [ 9000 > hash_rate ];  then 
 	/etc/init.d/cgminer reload
 fi
